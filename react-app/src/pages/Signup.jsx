ad2ff8923a871646090fe708175e2d9399194730
jhhest <janvanhest@outlook.com>
2020-03-09 15:17:53 +0100

    removed Fragment from Signup.jsx

--- react-app/src/pages/Signup.jsx
+++ react-app/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import SignupForm from "../components/SignupForm";
 
 export class Signup extends Component {
