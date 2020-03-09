8e803162df2f5a95e3026f1120fa5080b7dfa677
jhhest <janvanhest@outlook.com>
2020-03-09 15:04:38 +0100

    changed BrowserRouter routing

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import store from "./store/store"
@@ -11,9 +11,9 @@ import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
